74f9f63635037bf181f41a52ac02726782bcb901
SophieCylander <125442739+SophieCylander@users.noreply.github.com>
2023-02-17 10:47:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -213,8 +213,7 @@
           <div class="inner-content">
             <div class="text">
               <h4 class="h2">Contact us !</h4>
-              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Three categories of Partners are proposed: Premium/Silver/Gold. Thanks to contact us to obtain additional information.
 
             <div role="form" class="form" id="form" lang="en-US" dir="ltr">
               <form action="" method="post" class="" novalidate="novalidate" data-status="init">
