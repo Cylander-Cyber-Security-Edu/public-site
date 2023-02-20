bb587af25345c26133cf73e3361d36dcd639909e
Glenn ten Cate <glenntencate@gmail.com>
2023-02-20 10:30:02 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
-/bin/bash
+#!/bin/bash
 
 cd /tmp/
 git clone https://github.com/Cylander-Cyber-Security-Edu/public-site.git
 
 rm -rf /tmp/public-site/.git
+rm -rf /tmp/public-site/update.sh
 rm -rf /var/www/html/*
 cp -r /tmp/public-site/* /var/www/html
-rm -rf /tmp/public-site/.git
+rm -rf /tmp/public-site
