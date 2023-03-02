66938beaae93a6b368726718fde72903a592115e
blabla1337 <glenntencate@gmail.com>
2023-03-02 11:25:06 +0100

    update

---
+++ template-files/single.php
@@ -0,0 +1,31 @@
+<?php $class = "single" ?>
+<?php include "header.php" ?>
+
+<div id="site-content">
+	<!-- #main -->
+	<main id="main-content">
+		<article class="post type-post">
+			<div class="hero">
+				<div class="inner-hero">
+					<div class="breadcrumb"><a href="/index.php">Home</a> <span>GDPR</span></div>
+				</div><!-- .inner-hero -->
+			</div><!-- .hero -->
+
+			<div class="page-body">
+				<div class="wrapper-page-body">
+					<div class="inner-page-content">
+						<h1 class="page-title">Page gdpr</h1>
+						<div class="content text">
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea facilis non dolore sequi voluptas rerum exercitationem repellat laudantium reprehenderit quia minus expedita consequuntur obcaecati, nesciunt dolores adipisci sapiente ipsam nihil!</p>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea facilis non dolore sequi voluptas rerum exercitationem repellat laudantium reprehenderit quia minus expedita consequuntur obcaecati, nesciunt dolores adipisci sapiente ipsam nihil!</p>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea facilis non dolore sequi voluptas rerum exercitationem repellat laudantium reprehenderit quia minus expedita consequuntur obcaecati, nesciunt dolores adipisci sapiente ipsam nihil!</p>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea facilis non dolore sequi voluptas rerum exercitationem repellat laudantium reprehenderit quia minus expedita consequuntur obcaecati, nesciunt dolores adipisci sapiente ipsam nihil!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
+
+	</main><!-- #main -->
+</div>
+<?php include "footer.php" ?>
