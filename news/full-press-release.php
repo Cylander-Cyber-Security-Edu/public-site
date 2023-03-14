9cd06abd1f77b319099d62ae8104d988d968dde3
blabla1337 <glenntencate@gmail.com>
2023-03-14 13:44:24 +0100

    update news

---
+++ news/full-press-release.php
@@ -0,0 +1,56 @@
+<?php $class = "single single-post" ?>
+<?php include "header.php" ?>
+
+<div id="site-content">
+	<!-- #main -->
+	<main id="main-content">
+		<article class="post type-post">
+			<div class="hero">
+				<div class="inner-hero">
+					<div class="breadcrumb"><a href="/index">Home</a> <a href="/all-news">News</a> <span>Single news</span></div>
+					<header class="page-header">
+						<figure class="u-photo">
+							<img src="/news/img/defaut.jpg" alt="cylander img defaut">
+						</figure>
+					</header><!-- .page-header -->
+				</div><!-- .inner-hero -->
+			</div><!-- .hero -->
+
+			<div class="page-body">
+				<div class="wrapper-page-body">
+					<div class="inner-page-content">
+						<time>22/11/2021</time>
+						<h1 class="page-title">Press release announcing Cylander partnership against cybercrime</h1>
+						<div class="content text">
+							<p>Nearly 6 out of 10 organisations face a shortage of expertise</p>
+							<p>Cylander partner ING is proud to announce a unique partnership in the fight against cybercrime. ING, Microsoft and (OWASP) are set to launch a common digital platform aimed at training future experts in IT risks and security. The platform will go live in early 2022 with the support of the Open Source Security Foundation. The aim of this initiative is to remedy the structural shortage of skilled labour in the domain which, according to a recent study, impacts 57% of organisations in Belgium. The partnership was announced at the “Risk & Security in the New Normal” conference in the presence of Minister for Justice, Vincent Van Quickenborne.</p>
+							<p>Cybercrime currently affects all private and public organisations. The number of computer viruses and cyber-attacks has never been higher. Banking sector statistics showed that some 67,000 fraudulent phishing transactions were perpetrated in Belgium in 2020 at a cost of 34 million euros</p>
+							<p>The impact of this scourge weighs on the entire economy.</p>
+							<p>Philippe Ledent, economist at ING in Belgium</p>
+							<p>“The financial cost of cybercrime is no longer just impacting businesses but the global economy. Different sources estimate the cost of online crime on global GDP at USD 1 trillion,”</p>
+							<p>In order to fight this form of crime, organisations need IT risk and security specialists. However, a recent study has revealed that 57% of organisations face a shortage of skilled personnel in this domain. Three quarters (76%) of professionals questioned also mentioned having persistent difficulties in hiring qualified profiles. It is in this urgent context that the partners of Cylander have decided to join forces to launch a series of training materials aimed at preparing future IT risk and security experts to anticipate and counter cyber-attacks. This will take the form of a shared training platform to be launched by all partners in early 2022 and will be universally accessible. The content of the training will be designed by the partners themselves based on their experience and needs.</p>
+						</div>
+					</div>
+					<div class="share news-share">
+						<div class="share-name">Share this news : </div>
+						<ul class="social__link-list">
+							<li class="facebook">
+								<a target="_blank" href="#">facebook</a>
+							</li>
+							<li class="twitter">
+								<a target="_blank" href="#">twitter</a>
+							</li>
+							<li class="linkedin">
+								<a target="_blank" href="#">linkedin</a>
+							</li>
+							<li class="email"><a href="#">email</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</article>
+
+	</main><!-- #main -->
+</div>
+<?php include "footer.php" ?>
---
+++ news/press-release.php
@@ -0,0 +1,21 @@
+<article class="card">
+	<a href="/news/full-press-release" title="Read this article">
+		<header class="post-header">
+			<figure class="u-photo">
+				<img src="/img/defaut.jpg" alt="cylander img defaut">
+			</figure>
+		</header>
+
+		<div class="content">
+			<time>22/11/2021</time>
+			<h5 class="h6 title">Press release announcing Cylander partnership against cybercrime</h5>
+			<div class="post-content text" itemprop="description">
+			ING, the first bank in Europe to open its doors to external partnerships, announced during a conference that it will launch an open-source Risk and Security training track. ING collaborated with OWASP, SKF, Microsoft and others to enhance the risk and security knowledge of thousands of people globally. The training materials created will be freely available through well-known open-source platforms.
+			</div>
+
+			<footer>
+				<span class="fake-readmore">Read more</span>
+			</footer>
+		</div>
+	</a>
+</article>
